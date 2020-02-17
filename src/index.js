c4d2338715e62d1e6cd72fd74755f31d1626e93c
lkelly85 <41906906+lkelly85@users.noreply.github.com>
2020-02-17 15:52:59 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import "./init";
 // init is being called before the template is being rendered. Use the init function
 // to import Flourish modules that add properties to the state object
 
-export { default as data } from "./core/data";
+export var data = {};
 // If your template includes data tables, use this variable to access the data.
 // Each of the 'datasets' in data.json file will be available as properties of the data.
 
-export { default as state } from "./core/state";
-// The current state of template. You can make some or all of the properties
-// of the state object available to the user as settings in template.yml
+export var state = {
+  input: ""
+}
 
-export { default as draw } from "./core/draw";
+export function draw() {
+  g.select(".input")
+}
 // The draw function is called when the template first loads
 
-export { default as update } from "./core/update";
+export function update() {
+  g.select(".input")
+}
+  
 // The update function is called whenever the user changes a data table or settings
 // in the visualisation editor, or when changing slides in the story editor.
 
